e9349f92ebec34cd65fcf3470d53d8546e606324
Daniel Leal <85421049+zereflaw@users.noreply.github.com>
2021-06-29 23:22:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ document.getElementById('input').addEventListener('keydown', function (e) {
     var start = this.selectionStart
     var end = this.selectionEnd
 
-    // set textarea value to: text before caret + tab + text after caret
     this.value =
       this.value.substring(0, start) + '\t' + this.value.substring(end)
-
-    // put caret at right position again
+    
     this.selectionStart = this.selectionEnd = start + 1
   }
 })
